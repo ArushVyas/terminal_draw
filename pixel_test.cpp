e74d0597eee85ae25de80ab7c9d7bc69535e1ce6
ArushVyas <140494596+ArushVyas@users.noreply.github.com>
2025-06-11 16:29:51 +0530

    Delete pixel_test.cpp

--- pixel_test.cpp
+++
@@ -1,9 +0,0 @@
-// I personally use this file for testing and playing around with pixel.hpp
-#include <stdio.h>
-#include <c:\Users\ppp\Desktop\pixel_drawing\pixel.hpp>
-
-
-int main() {
-    
-    getchar();
-}
